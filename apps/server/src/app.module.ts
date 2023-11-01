c10761fd84d25be3980422acd97e3fa0442da455
Dmitriy <dima.komlyk@gmail.com>
2023-11-01 11:12:08 +0200

    feat: implemented trpc for server side

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TrpcModule } from '@server/trpc/trpc.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), TrpcModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ apps/server/src/trpc/trpc.router.ts
@@ -0,0 +1,29 @@
+import { INestApplication, Injectable } from '@nestjs/common';
+import { TrpcService } from '@server/trpc/trpc.service';
+import * as trpcExpress from '@trpc/server/adapters/express';
+import { z } from 'zod';
+
+@Injectable()
+export class TrpcRouter {
+  constructor(private readonly trpc: TrpcService) {}
+
+  // Merge routers together
+  appRouter = this.trpc.router({
+    by: this.trpc.procedure
+      .input(z.object({ name: z.string().optional() }))
+      .query(({ input }) => {
+        return `FullStack Boilerplate by ${
+          input.name ? input.name : `Dmytro Komlyk`
+        }`;
+      }),
+  });
+
+  async applyMiddleware(app: INestApplication) {
+    app.use(
+      process.env.API,
+      trpcExpress.createExpressMiddleware({ router: this.appRouter }),
+    );
+  }
+}
+
+export type AppRouter = TrpcRouter['appRouter'];
