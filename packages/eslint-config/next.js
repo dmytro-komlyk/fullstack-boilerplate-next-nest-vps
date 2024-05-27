35270532afd646ceae0993c71465650ff93d3e56
Dmitriy <dima.komlyk@gmail.com>
2024-05-27 11:31:18 +0300

    build: added eslint package

---
+++ packages/eslint-config/next.js
@@ -0,0 +1,124 @@
+const { resolve } = require('node:path');
+
+const project = resolve(process.cwd(), 'tsconfig.json');
+
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  extends: ['airbnb-base', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  globals: {
+    React: true,
+    JSX: true,
+  },
+  env: {
+    es6: true,
+    node: true,
+  },
+  plugins: ['react', '@typescript-eslint'],
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project,
+      },
+    },
+  },
+  ignorePatterns: [
+    // Ignore dotfiles
+    '.*.js',
+    'node_modules/',
+    'dist/',
+    '.next/',
+  ],
+  rules: {
+    'no-underscore-dangle': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        endOfLine: 'auto',
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ['**/*.js?(x)', '**/*.ts?(x)'],
+      plugins: [
+        '@typescript-eslint',
+        'unused-imports',
+        'tailwindcss',
+        'simple-import-sort',
+      ],
+      extends: [
+        'plugin:tailwindcss/recommended',
+        'airbnb',
+        'next/core-web-vitals',
+        'plugin:prettier/recommended',
+      ],
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+      rules: {
+        'no-underscore-dangle': 'off',
+        'no-param-reassign': 0,
+        'prettier/prettier': [
+          'error',
+          {
+            singleQuote: true,
+            endOfLine: 'auto',
+          },
+        ],
+        'react/function-component-definition': 'off', // Disable Airbnb's specific function type
+        'react/destructuring-assignment': 'off', // Vscode doesn't support automatically destructuring, it's a pain to add a new variable
+        'react/require-default-props': 'off', // Allow non-defined react props as undefined
+        'react/jsx-props-no-spreading': 'off', // _app.tsx uses spread operator and also, react-hook-form
+        'react/jsx-filename-extension': [
+          1,
+          { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+        ],
+        '@next/next/no-img-element': 'off', // We currently not using next/image because it isn't supported with SSG mode
+        '@typescript-eslint/comma-dangle': 'off', // Avoid conflict rule between Eslint and Prettier
+        '@typescript-eslint/no-explicit-any': 'warn',
+        '@typescript-eslint/consistent-type-imports': 'error', // Ensure `import type` is used when it's necessary
+        'no-restricted-syntax': [
+          'error',
+          'ForInStatement',
+          'LabeledStatement',
+          'WithStatement',
+        ], // Overrides Airbnb configuration and enable no-restricted-syntax
+        'import/prefer-default-export': 'off', // Named export is easier to refactor automatically
+        'import/extensions': [
+          'error',
+          'ignorePackages',
+          {
+            js: 'never',
+            jsx: 'never',
+            ts: 'never',
+            tsx: 'never',
+          },
+        ],
+        'simple-import-sort/imports': 'error', // Import configuration for `eslint-plugin-simple-import-sort`
+        'simple-import-sort/exports': 'error', // Export configuration for `eslint-plugin-simple-import-sort`
+        'tailwindcss/no-custom-classname': 'off',
+        'jsx-a11y/label-has-associated-control': [
+          'off',
+          {
+            labelComponents: ['label'],
+            controlComponents: ['input'],
+          },
+        ],
+        'jsx-a11y/click-events-have-key-events': 'off',
+        'jsx-a11y/no-static-element-interactions': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+        'unused-imports/no-unused-imports': 'error',
+        'unused-imports/no-unused-vars': [
+          'warn',
+          {
+            vars: 'all',
+            varsIgnorePattern: '^_',
+            args: 'after-used',
+            argsIgnorePattern: '^_',
+          },
+        ],
+      },
+    },
+  ],
+};
