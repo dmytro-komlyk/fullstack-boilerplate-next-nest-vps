7d960ed86c33d12f821f336254a5e6a237de29e1
Dmitriy <dima.komlyk@gmail.com>
2024-05-28 18:16:49 +0300

    refactor: updated env

--- apps/admin/app/(lib)/constants.ts
+++ apps/admin/app/(lib)/constants.ts
@@ -3,8 +3,7 @@ export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL as string;
 export const DOCKER_SERVICE_URL = process.env
   .NEXT_PUBLIC_DOCKER_SERVICE_URL as string;
 export const SERVER_API_URL = process.env.NEXT_PUBLIC_SERVER_API_URL as string;
-export const SERVER_TRPC_URL = process.env
-  .NEXT_PUBLIC_SERVER_TRPC_URL as string;
+export const SERVER_TRPC_URL = 'http://134.249.168.21:3000/api/trpc';
 export const AUTH_URL = process.env.NEXTAUTH_URL as string;
 export const AUTH_JWT_ACCESS_TOKEN_EXPIRATION = process.env
   .NEXTAUTH_JWT_ACCESS_TOKEN_EXPIRATION as string;
