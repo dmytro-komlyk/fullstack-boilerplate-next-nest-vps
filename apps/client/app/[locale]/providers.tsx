2e06ca9a4b010cf3002a1838e2319bd4696e02b1
Dmitriy <dima.komlyk@gmail.com>
2024-06-16 14:22:07 +0300

    refactor: fix async error

--- apps/client/app/[locale]/providers.tsx
+++ apps/client/app/[locale]/providers.tsx
@@ -5,7 +5,7 @@ import * as React from 'react';
 
 import { TrpcProvider } from '../(utils)/trpc/Provider';
 
-export async function Providers({ children }: { children: React.ReactNode }) {
+export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <TrpcProvider>
       <NextUIProvider>{children}</NextUIProvider>
